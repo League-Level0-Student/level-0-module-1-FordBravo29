af30f52bd86360f1fd4ddeb6ada8dd9a44f39c4d
11000783 <josephaguilar2022@gmail.com>
2019-01-27 11:30:29 -0800

    badger

---
+++ src/Badgers.java
@@ -0,0 +1,58 @@
+import javax.swing.JOptionPane;
+
+public class Badgers {
+	public static void main(String[] args) {
+	String hi= "Badger";	
+	String hello="MUSHROOM";
+	String h="A SNAKE A SNAKE SNAAAKE! SNAAAKE!\"\n" + 
+			"		+ \"Ooo SNAKE";
+	
+	for(int i=0; i<16;i++) {
+			JOptionPane.showMessageDialog(null, hi );
+		}
+			for(int i=0; i<2;i++) {	
+			JOptionPane.showMessageDialog(null, hello );
+		
+	}
+for(int i=0; i<16;i++) {
+	JOptionPane.showMessageDialog(null, hi );
+	
+}for(int i=0; i<2;i++) {	
+		JOptionPane.showMessageDialog(null, hello);
+
+}for(int i=0; i<16;i++) {
+			JOptionPane.showMessageDialog(null, hi );
+			
+}for(int i=0; i<2;i++) {	
+	JOptionPane.showMessageDialog(null, hello);
+
+}for(int i=0; i<16;i++) {
+	JOptionPane.showMessageDialog(null, hi );
+}
+JOptionPane.showMessageDialog(null, h);
+
+for(int i=0; i<16;i++) {
+	JOptionPane.showMessageDialog(null, hi );
+	
+}for(int i=0; i<2;i++) {	
+		JOptionPane.showMessageDialog(null, hello);
+
+}for(int i=0; i<16;i++) {
+			JOptionPane.showMessageDialog(null, hi );
+			
+}for(int i=0; i<2;i++) {	
+	JOptionPane.showMessageDialog(null, hello);
+
+}for(int i=0; i<16;i++) {
+	JOptionPane.showMessageDialog(null, hi );
+}
+JOptionPane.showMessageDialog(null, h);
+
+
+
+}}
+	
+
+	
+
+
